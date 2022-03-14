5affc8094fa57e0f8fb0c06531c56e900290a6aa
timothyAgevi <timothyagevi@gmail.com>
2022-03-14 16:38:10 +0300

    bets and wager on mjs

--- rockPaperXcissors/index.mjs
+++ rockPaperXcissors/index.mjs
@@ -1,41 +1,47 @@
-import {loadStdlib}from '@reach-sh/stdlib';
-import * as backend from './build/index.default.mjs';
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+const stdlib = loadStdlib(process.env);
 
-const stdlib =loadStdlib(process.env);
+const startingBalance = stdlib.parseCurrency(100);
+const accAlice = await stdlib.newTestAccount(startingBalance);
+const accBob = await stdlib.newTestAccount(startingBalance);
 
-(async ()=>{
-const startingBalance=stdlib.parseCurrency(10);
-//accounts
-const accAlice=await stdlib.newTestAccount(startingBalance)
-const accBob=await stdlib.newTestAccount(startingBalance)
-//contracts
-const ctcAlice =accAlice.contract(backend)
-const ctcBob =accAlice.contract(backend,ctcAlice.getInfo())
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
+const beforeAlice = await getBalance(accAlice);
+const beforeBob = await getBalance(accBob);
 
-//player interface 0 ,1,2
-const HAND =['Rock','Paper','Scissors']
-const OUTCOME =['Bob wins','Draw','Alice wins']
-//logic to play game ,use constructor to hve player object for both
-const Player=(who)=>({
-    getHand:()=>{
-      const hand =Math.floor(Math.random()*3)
-      console.log(`${who} played${HAND[hand]}`)//HAND to hand
-      return hand
-    },
-    seeOutcome:(outcome)=>{
-       console.log(`${who} saw outcome ${OUTCOME[outcome]}`)
-    }
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
-})
+const HAND = ['Rock', 'Paper', 'Scissors'];
+const OUTCOME = ['Bob wins', 'Draw', 'Alice wins'];
+const Player = (Who) => ({
+  getHand: () => {
+    const hand = Math.floor(Math.random() * 3);
+    console.log(`${Who} played ${HAND[hand]}`);
+    return hand;
+  },
+  seeOutcome: (outcome) => {
+    console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+  },
+});
 
 await Promise.all([
-    backend.Alice(ctcAlice ,{
-    ...Player('Alice')
-    }),
-    backend.Bob(ctcBob,{
-        //implements Bob interact object
-        ...Player('Alice'),
-    })
-])
+  ctcAlice.p.Alice({
+    ...Player('Alice'),
+    wager: stdlib.parseCurrency(5),
+  }),
+  ctcBob.p.Bob({
+    ...Player('Bob'),
+    acceptWager: (amt) => {
+      console.log(`Bob accepts the wager of ${fmt(amt)}.`);
+    },
+  }),
+]);
 
-}) ();
+const afterAlice = await getBalance(accAlice);
+const afterBob = await getBalance(accBob);
+
+console.log(`Alice went from ${beforeAlice} to ${afterAlice}.`);
+console.log(`Bob went from ${beforeBob} to ${afterBob}.`);
