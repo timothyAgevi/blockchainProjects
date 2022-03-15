acf16dc2c6081e6ecf00d97954985468df16f2e3
timothyAgevi <timothyagevi@gmail.com>
2022-03-15 22:21:36 +0300

    create accounts

--- rockPaperXcissors/index.mjs
+++ rockPaperXcissors/index.mjs
@@ -1,7 +1,7 @@
 import { loadStdlib } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
 const stdlib = loadStdlib(process.env);
-
+//create accounts
 const startingBalance = stdlib.parseCurrency(100);
 const accAlice = await stdlib.newTestAccount(startingBalance);
 const accBob = await stdlib.newTestAccount(startingBalance);
