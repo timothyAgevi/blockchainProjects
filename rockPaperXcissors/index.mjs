a4d526b40849e515066dd213249b660a68595699
timothyAgevi <timothyagevi@gmail.com>
2022-03-17 21:09:47 +0300

    set timeout for both participants

--- rockPaperXcissors/index.mjs
+++ rockPaperXcissors/index.mjs
@@ -28,7 +28,7 @@ const Player = (Who) => ({
   getHand: async () => { // <-- async now
     const hand = Math.floor(Math.random() * 3);
     console.log(`${Who} played ${HAND[hand]}`);
-    if ( Math.random() <= 0.01 ) {
+    if ( Math.random() <= 0.01 ) {//change the threshold so that timeouts only happen 1% of the time
       for ( let i = 0; i < 10; i++ ) {
         console.log(`  ${Who} takes their sweet time sending it back...`);
         await stdlib.wait(1);
@@ -54,15 +54,8 @@ await Promise.all([
   ctcBob.p.Bob({//instead of backend.Bob
     ...Player('Bob'),
     //Lines 42 through 51 redefine Bob's acceptWager method as an asynchronous function, where half of the time it will take at least ten blocks on the Ethereum network by waiting for ten units of time to pass. We know that ten is the value of deadline, so this will cause a timeout
-    acceptWager: async (amt) => { // <-- async now
-      if ( Math.random() <= 0.5 ) {
-        for ( let i = 0; i < 10; i++ ) {
-          console.log(`  Bob takes his sweet time...`);
-          await stdlib.wait(1);
-        }
-      } else {
-        console.log(`Bob accepts the wager of ${fmt(amt)}.`);
-      }
+    acceptWager: (amt) => {
+      console.log(`Bob accepts the wager of ${fmt(amt)}.`);
     },
   
 
