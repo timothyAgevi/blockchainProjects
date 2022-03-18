201bd6f0d2053cf4aa007b5ced1368056af4abb1
timothyAgevi <timothyagevi@gmail.com>
2022-03-18 11:56:50 +0300

    define app component on mount

--- rockPaperXcissors/index.js
+++ rockPaperXcissors/index.js
@@ -19,6 +19,15 @@ class App extends React.Component{
         super (props);this.state={view:'ConnectAccount',...defaults};
     }
     async componentDidMount(){
+        const acc= await reach.getDefaultAccount();
+        const balAtomic=await reach.balanceOf(acc);
+        const bal=reach.formatCurrency(balAtomic,4);
+        this .setState({acc,bal});
+        if(await reach.canFundFromFaucet()){
+            this.setState({view:'FundAccount'});
+        }else{
+            this.setState({view:'DeployerOrAttacher'});
+        }
 
     }
 }
