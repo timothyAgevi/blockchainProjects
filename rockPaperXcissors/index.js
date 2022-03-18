915d7cff43a0c0ccea0499a00716d240326a16be
timothyAgevi <timothyagevi@gmail.com>
2022-03-18 11:40:25 +0300

    add helper functions and defaults

--- rockPaperXcissors/index.js
+++ rockPaperXcissors/index.js
@@ -7,4 +7,9 @@ import './index.css';
 import * as backend from './build/index.main.mjs';
 //std library
 import {loadStdlib} from '@reach-sh/stdlib';
-const reach = loadStdlib(process.env);
+const reach = loadStdlib(process.env);//process.env. are variables used to acess desired stdlib 
+//helper functions and defaults
+const handToInt = {'ROCK': 0, 'PAPER': 1, 'SCISSORS': 2};
+const intToOutcome = ['Bob wins!', 'Draw!', 'Alice wins!'];
+const {standardUnit} = reach;
+const defaults = {defaultFundAmt: '10', defaultWager: '3', standardUnit};
