05c0eda30ef7a4e90b8fcdcf4afe48efacd39b59
timothyAgevi <timothyagevi@gmail.com>
2022-03-18 11:45:18 +0300

    update defaults

--- rockPaperXcissors/index.js
+++ rockPaperXcissors/index.js
@@ -13,3 +13,8 @@ const handToInt = {'ROCK': 0, 'PAPER': 1, 'SCISSORS': 2};
 const intToOutcome = ['Bob wins!', 'Draw!', 'Alice wins!'];
 const {standardUnit} = reach;
 const defaults = {defaultFundAmt: '10', defaultWager: '3', standardUnit};
+
+
+
+
+
