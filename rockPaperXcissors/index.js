c9e0aca81a243b1811fead1da3c399f124761a16
timothyAgevi <timothyagevi@gmail.com>
2022-03-18 11:28:50 +0300

    add imports ti index.js

---
+++ rockPaperXcissors/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppViews from './views/AppViews';
+import DeployerViews from './views/DeployerViews';
+import AttacherViews from './views/AttacherViews';
+import {renderDOM, renderView} from './views/render';
+import './index.css';
+import * as backend from './build/index.main.mjs';
+import {loadStdlib} from '@reach-sh/stdlib';
+const reach = loadStdlib(process.env);
