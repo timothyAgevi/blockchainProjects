c84b481ebce1aaa29f0d0d4787f18f31af7b3edf
timothyAgevi <timothyagevi@gmail.com>
2022-03-18 11:49:57 +0300

    define asyc fun for on mount

--- rockPaperXcissors/index.js
+++ rockPaperXcissors/index.js
@@ -18,6 +18,9 @@ class App extends React.Component{
     constructor(props){
         super (props);this.state={view:'ConnectAccount',...defaults};
     }
+    async componentDidMount(){
+
+    }
 }
 
 
