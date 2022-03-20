96b8c971487b0c1e1b93a315480e0f3d2b3804ba
timothyAgevi <timothyagevi@gmail.com>
2022-03-20 19:31:47 +0300

    run reach program as bob

--- rockPaperXcissors/index.js
+++ rockPaperXcissors/index.js
@@ -83,7 +83,7 @@ class App extends React.Component{
             attach(ctcInfoStr) {
               const ctc = this.props.acc.contract(backend, JSON.parse(ctcInfoStr));
               this.setState({view: 'Attaching'});//set the component state to display Attaching display
-              backend.Bob(ctc, this);
+              backend.Bob(ctc, this);//run reach program as bob
             }
             async acceptWager(wagerAtomic) { // Fun([UInt], Null)
               const wager = reach.formatCurrency(wagerAtomic, 4);
